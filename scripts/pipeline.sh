62f35c44c44610ce8db50a70a4141a9cb86683ff
Guadalupe <guadalupecominom@gmail.com>
2025-04-12 23:14:44 +0200

    código cutadapt y STAR sin el archivo pipeline.log

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -20,26 +20,34 @@ done
 # TODO: run cutadapt for all merged files
 # cutadapt -m 18 -a TGGAATTCTCGGGTGCCAAGG --discard-untrimmed \
 #     -o <trimmed_file> <input_file> > <log_file>
-
-for sid  in out/merged/*_sRNA.fastq 
+mkdir out/cutadapt
+mkdir log/cutadapt
+for file  in out/merged/*.fastq.gz 
 do
-    sampleid=$(basename $sid .fastq)
+    sampleid=$(basename $file .fastq.gz)
     
     cutadapt -m 18 \
         -a TGGAATTCTCGGGTGCCAAGG \
         --discard-untrimmed \
-        -o out/cutadapt/${sampleid}_trimmed.fastq \
-        $sid  > log/cutadapt/${sampleid}.log
+        -o out/cutadapt/${sampleid}_trimmed.fastq.gz \
+        $file  > log/cutadapt/${sampleid}.log
+
+	#Include cutadapt's results in pipeline.log
+	echo "Trimming Results" >>log/pipeline.log
+	echo "Processing sample $sampleid with cutadapt" >> log/pipeline.log
+	log/cutadapt/${sampleid}.log >> log/pipeline.log
+	echo -e "==============================================================="
+
 
 # TODO: run STAR for all trimmed files
 for fname in out/trimmed/*.fastq.gz
 do
     # you will need to obtain the sample ID from the filename
-    sid=#TODO
-    # mkdir -p out/star/$sid
-    # STAR --runThreadN 4 --genomeDir res/contaminants_idx \
-    #    --outReadsUnmapped Fastx --readFilesIn <input_file> \
-    #    --readFilesCommand gunzip -c --outFileNamePrefix <output_directory>
+    sid=$(basename $fname .fastq.gz)
+    mkdir -p out/star/$sid
+    STAR --runThreadN 4 --genomeDir res/contaminants_idx \
+         --outReadsUnmapped Fastx --readFilesIn $fname \
+         --readFilesCommand gunzip -c --outFileNamePrefix out/star/$sid
 done 
 
 # TODO: create a log file containing information from cutadapt and star logs
